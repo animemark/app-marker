8d4fb1ccfd7a0c099b50aec55df351d8baefc2d6
Larry Kwan <i@gyz.me>
2021-02-15 00:12:20 +0800

    xxx

--- config-overrides.js
+++ config-overrides.js
@@ -2,17 +2,17 @@ const multipleEntry = require('react-app-rewire-multiple-entry')([
   {
     entry: 'src/entry/discuss.PostList.js',
     template: 'src/entry/template.html',
-    outPath: '/discuss.PostList.html'
+    outPath: '/discuss/PostList.html'
   },
   {
     entry: 'src/entry/marking.EditMark.js',
     template: 'src/entry/template.html',
-    outPath: '/marking.EditMark.html'
+    outPath: '/marking/EditMark.html'
   },
   {
     entry: 'src/entry/marking.MarkList.js',
     template: 'src/entry/template.html',
-    outPath: '/marking.MarkList.html'
+    outPath: '/marking/MarkList.html'
   },
 ]);
 
